2da2aaf3f3c206103338cc03903ad4e8ba04eb1d
brunobazzan <brunobazzan@gmail.com>
2020-03-04 11:00:31 -0300

    Update piezo.py

--- piezo.py
+++ piezo.py
@@ -42,7 +42,6 @@ plt.plot(f_res, t_res, '.')
 plt.grid(True)
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('ransferencia')
-plt.legend()
 plt.show()
 
 data_anti = pd.read_csv('Antiresonancia.csv')
@@ -90,6 +89,7 @@ R_0 = (R_2/T_0) - R_2
 L_0 = (Q_0*R_2)/(w_s_0*T_0)
 C_0 = 1/(L_0*w_s_0**2)
 
+'''
 def func_trans_res(f, R, L, C):
     return R_2/(np.sqrt((R_2 + R)**2 + (2*np.pi*f*L - 1/(2*np.pi*f*C))**2))
 
@@ -99,29 +99,30 @@ plt.grid(True)
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Transferencia')
 plt.show()
-    
-params_1, cov_1 = curve_fit(lambda f, R: func_trans_res(f, R, L_0, C_0), f_campana, t_campana, [R_0], bounds=(0,np.inf))
+'''    
+#params_1, cov_1 = curve_fit(lambda f, R: func_trans_res(f, R, L_0, C_0), f_campana, t_campana, [R_0], bounds=(0,np.inf))
 
-print(params_1)
+#print(params_1)
 
-params_2, cov_2 = curve_fit(lambda f, L, C: func_trans_res(f, params_1, L, C), f_campana, t_campana, [L_0, C_0], bounds=(0, np.inf))
+#params_2, cov_2 = curve_fit(lambda f, L, C: func_trans_res(f, params_1, L, C), f_campana, t_campana, [L_0, C_0], bounds=(0, np.inf))
 
-print(params_2)
+#print(params_2)
 
 def func_trans_res_v2(f, T, w_s, Q):
     return R_2/(np.sqrt((R_2/T)**2 + ((2*np.pi*f*Q*R_2)/(w_s*T)-(Q*R_2*w_s)/(T*2*np.pi*f))**2))
 
+'''
 figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
 plt.plot(f_campana, func_trans_res_v2(f_campana, T_0, w_s_0, Q_0))
 plt.grid(True)
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Transferencia')
 plt.show()
-
+'''
 params_3, cov_3 = curve_fit(func_trans_res_v2, f_campana, t_campana, [T_0, w_s_0, Q_0], bounds=(0, np.Inf))
 
-print(params_3)
-print(T_0, w_s_0, Q_0)
+print('Parametros: ', params_3)
+print('Estimaciones: ', T_0, w_s_0, Q_0)
 
 figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
 plt.plot(f_campana, t_campana, '.', label='T(f)')
@@ -131,3 +132,39 @@ plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Transferencia')
 plt.legend()
 plt.show()
+
+#%%
+camp_res = pd.read_csv('Campana resonancia fina(1)', header=None)
+camp_res = camp_res.transpose()
+camp_res.columns = ['Frecuencia', 'V_out', 'Fase']
+f = camp_res.iloc[:,0].to_numpy()
+v_out = camp_res.iloc[:,2].to_numpy()
+phi = camp_res.iloc[:,1].to_numpy()
+v_in = np.zeros(len(v_out))
+for i in range(len(v_out)):
+    v_in[i] = 1/np.sqrt(2)
+Trans = v_out/v_in
+
+R_2_2 = 10000
+w_s_0_2 = 2*np.pi*f[4752]
+w_p_0_2 = 2*np.pi*f[4877]
+w_m_0_2 = 2*np.pi*f[4625]
+Q_0_2 = w_s_0_2/(w_p_0_2 - w_m_0_2)
+T_0_2 = max(Trans)
+R_0_2 = (R_2_2/T_0_2) - R_2_2
+L_0_2 = (Q_0_2*R_2_2)/(w_s_0_2*T_0_2)
+C_0_2 = 1/(L_0_2*w_s_0_2**2)
+
+params_4, cov_4 = curve_fit(func_trans_res_v2, f, Trans, [T_0, w_s_0, Q_0])
+print('Parametros: ', params_4)
+print('Estimaciones: ', T_0, w_s_0, Q_0)
+
+figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
+plt.plot(f, Trans, '.', label='T(f)')
+plt.plot(f, func_trans_res_v2(f, params_4[0], params_4[1], params_4[2]), label='Ajuste')
+plt.xlabel('Frecuencia (Hz)')
+plt.ylabel('Transferencia')
+plt.grid(True)
+plt.legend()
+plt.show()
+
